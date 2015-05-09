875a1abf05028e4040f88d368660fdb35f5a3cdf
Ladvien <design@ubermentis.com>
2015-05-09 09:26:00 -0500

    Bloggin

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -16,13 +16,17 @@ $(document).ready(function(){
     });
 
     $( "#night-image" ).hover(function() {
+
       $( "#night-image" ).animate({
         opacity: 0//,
         //left: "+=50",
         //height: "toggle"
-      }, 100, function() {
+      }, 300, function() {
         hidden: true
       });
+
+
+
     });
 
 
@@ -31,8 +35,18 @@ $(document).ready(function(){
 
 function slideImageUp() {
     $( "#day-image" ).animate({opacity: 0})
+    $(".hidden-text").animate({
+      opacity: 1.0
+    }, 300, function() {
+
+    });
 };
 
 function slideImageDown() {
   $( "#day-image" ).animate({opacity: 1})
+  $(".hidden-text").animate({
+    opacity: 0.0
+  }, 300, function() {
+
+  });
 };

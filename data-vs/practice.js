f285d453ebf0cb1dca1be93a7e4e078a424386d2
Thomas Brittain <honeysucklehardware@gmail.com>
2017-05-16 21:29:15 -0500

    d

--- data-vs/practice.js
+++ data-vs/practice.js
@@ -1,4 +1,4 @@
 
-document.querySelector('#btn-1').addEventListener('click', function(){
+document.getElementById('btn-1').addEventListener('click', function(){
 	var diceDOM = document.querySelector('#dice').textContent = "YOU CLICKED IT!!";	
 });

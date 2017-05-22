1cb1473d2b26097ebeec13f7cecf732c35037f28
Thomas Brittain <honeysucklehardware@gmail.com>
2017-05-22 08:00:16 -0500

    Added terminal text highlighting

--- javascripts/js-ble.js
+++ javascripts/js-ble.js
@@ -4,54 +4,48 @@ var writeCharacteristic;
 var descriptor;
 var receivedString = "";
 var terminalLineCounter = 0;
+var displayDOM = 'terminal';
 
 function onScanButtonClick() {
   // Validate services UUID entered by user first.
   let optionalServices = document.getElementById('optionalServices').value
     .split(/, ?/).map(s => s.startsWith('0x') ? parseInt(s) : s)
     .filter(s => s && BluetoothUUID.getService);
-    
-  log(optionalServices);
 
-  log('Requesting any Bluetooth Device...');
+  addSystemText('Requesting any Bluetooth Device...');
+
   navigator.bluetooth.requestDevice({
       acceptAllDevices: true,
       optionalServices: optionalServices})
   .then(device => {
-    log(device)
-    log('Connecting to GATT Server...');
+    addSystemText('Connecting to GATT Server...');
     return device.gatt.connect();
   })
   .then(server => {
-    log("Printing server info: ");
-    log(server);
-    log('Getting Services...');
+    addSystemText('Getting Services...');
     return server.getPrimaryServices(0xFFE0);
   })
 .then(services =>{
-      log("Found services: ")
-      log(services);
-      
-        services.forEach(service => {
-            let queue = Promise.resolve();
-          queue = queue.then(_ => service.getCharacteristics().then(characteristics => {
-            log('> Service: ' + service.uuid);
-            characteristics.forEach(characteristic => {
-              log('>> Characteristic: ' + characteristic.uuid + ' ' +
-                  getSupportedProperties(characteristic));
-                writeCharacteristic = characteristic;
-                writeCharacteristic.addEventListener('characteristicvaluechanged', onReceivedData);
-                writeCharacteristic.startNotifications();
-                characteristic.getDescriptors('gatt.characteristic_user_description').then(descriptors => {
-                    descriptor = descriptors[0];
-                    log("Write descriptor set.");
-                });
+    addSystemText("Found services: ")  
+    services.forEach(service => {
+    let queue = Promise.resolve();
+    queue = queue.then(_ => service.getCharacteristics().then(characteristics => {
+        addSystemText('> Service: ' + service.uuid);
+        characteristics.forEach(characteristic => {
+          addSystemText('>> Characteristic: ' + characteristic.uuid + ' ' +
+              getSupportedProperties(characteristic));
+            writeCharacteristic = characteristic;
+            writeCharacteristic.addEventListener('characteristicvaluechanged', onReceivedData);
+            writeCharacteristic.startNotifications();           characteristic.getDescriptors('gatt.characteristic_user_description').then(descriptors => {
+                descriptor = descriptors[0];
+                addSystemText("Write descriptor set");
             });
-          }));
-      })
+        });
+      }));
   })
+})
 .catch(error => {
-    log('Argh! ' + error);
+    addSystemText('Argh! ' + error);
   });
 }
 
@@ -59,13 +53,7 @@ function onReceivedData(event){
     for(var i = 0; i < event.target.value.byteLength; i ++){
         receivedString += String.fromCharCode(event.target.value.getUint8(i));
     }
-    var terminal = document.getElementById('terminal')
-    var newLine = document.createElement('div');
-    newLine.innerHTML = "<- " + receivedString;
-    newLine.classList.add('received-text');
-    terminalLineCounter++;
-    terminal.appendChild(newLine);
-    terminal.scrollTop = terminal.scrollHeight;
+    addTerminalLine(displayDOM, receivedString, '<- ', 'received-text');
     receivedString = "";
 }
 
@@ -75,8 +63,7 @@ function onWriteButtonClick(){
         let textToWrite = document.getElementById('textToWrite').value;
         writeCharacteristic.writeValue(encoder.encode(textToWrite))
         .then(_ => {
-            log("Sent: " + textToWrite);
-            addTerminalLine('terminal', textToWrite, '-> ', 'sent-text');
+            addTerminalLine(displayDOM, textToWrite, '-> ', 'sent-text');
         })
     }
 }
@@ -92,7 +79,7 @@ function addTerminalLine(displayElement, text, pretext, lineStyle){
     
     var terminal = document.getElementById(displayElement);
     var newLine = document.createElement('div');
-    newLine.innerHTML = pretext + textToWrite;
+    newLine.innerHTML = pretext + text;
     if(lineStyle != "") { 
        newLine.classList.add(lineStyle); 
     }
@@ -102,6 +89,10 @@ function addTerminalLine(displayElement, text, pretext, lineStyle){
 }
 
 /* Utils */
+
+function addSystemText(text){
+    addTerminalLine(displayDOM, text, '-) ', 'system-text');
+}
 
 function getSupportedProperties(characteristic) {
   let supportedProperties = [];
@@ -117,6 +108,6 @@ document.getElementById('btn-1').onclick = onScanButtonClick;
 document.getElementById('btn-write-ble').onclick = onWriteButtonClick;
 
 
-TSB function (){
-    
-}
+//TSB function (){
+//    
+//}

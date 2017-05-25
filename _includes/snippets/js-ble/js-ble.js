8e9790e48675320ea67cb47bc24e65b8b4f7ba2a
Thomas Brittain <honeysucklehardware@gmail.com>
2017-05-25 10:33:08 -0500

    r

---
+++ _includes/snippets/js-ble/js-ble.js
@@ -0,0 +1,51 @@
+//https://developer.mozilla.org/en-US/docs/Web/API/BluetoothRemoteGATTServer
+var log = console.log;
+//var writeCharacteristic;
+var descriptor;
+var receivedString = "";
+var terminalLineCounter = 0;
+var displayDOM = 'terminal';
+
+let primaryService = document.getElementById('optionalServices').value;
+
+var terminal = new Terminal(displayDOM);
+
+function onScanButtonClick() {
+    lumiBle.searchAndConnect(0xFFE0).
+    then(() => {
+        lumiBle.addReceivedDataCallback(onReceivedData)     
+    })
+}
+
+function onReceivedData(event){
+    for(var i = 0; i < event.target.value.byteLength; i ++){
+        receivedString += String.fromCharCode(event.target.value.getUint8(i));
+    }
+    terminal.addTerminalLine(displayDOM, receivedString, '<- ', 'received-text');
+    receivedString = "";
+}
+
+function onWriteButtonClick(){
+    let textToWrite = document.getElementById('textToWrite').value;
+//        writeCharacteristic.writeValue(encoder.encode(textToWrite))
+    lumiBle.writeData(textToWrite)
+    .then(_ => {
+        terminal.addTerminalLine(displayDOM, textToWrite, '-> ', 'sent-text');
+    })
+}
+
+/* Utils */
+function getSupportedProperties(characteristic) {
+  let supportedProperties = [];
+  for (const p in characteristic.properties) {
+    if (characteristic.properties[p] === true) {
+      supportedProperties.push(p.toUpperCase());
+    }
+  }
+  return '[' + supportedProperties.join(', ') + ']';
+}
+
+document.getElementById('btn-1').onclick = onScanButtonClick;
+document.getElementById('btn-write-ble').onclick = onWriteButtonClick;
+
+var lumiBle = new LumiBluetooth();
---
+++ _includes/snippets/js-ble/terminal.js
@@ -0,0 +1,34 @@
+// Terminal JavaScript.
+// A webbased terminal emulator
+
+
+var Terminal = function (displayDOM) {
+    
+    var self = this;
+    this.displayDOM = displayDOM;
+    this.terminalLineCounter = 0;
+        
+    this.addTerminalLine = function (displayElement, text, pretext, lineStyle) {
+
+        // 1. Get the element to display text on.
+        // 2. Create a div for the new line.
+        // 3. Concactenate pre-text and text.
+        // 4. Check for style, apply
+        // 5. Append the new line DIV to target element.
+        // 6. Increment line count.
+
+        var terminal = document.getElementById(displayElement);
+        var newLine = document.createElement('div');
+        newLine.innerHTML = pretext + text;
+        if (lineStyle !== "") {
+            newLine.classList.add(lineStyle);
+        }
+        terminal.appendChild(newLine);
+        terminal.scrollTop = terminal.scrollHeight;
+        terminalLineCounter++;
+    };
+    
+    this.addSystemText = function (text){
+        self.addTerminalLine(displayDOM, text, '-) ', 'system-text');
+    }
+};

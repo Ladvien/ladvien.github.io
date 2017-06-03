1443d53062638e4eda4bf092e75a0681141fbfdf
Thomas Brittain <honeysucklehardware@gmail.com>
2017-06-02 22:09:48 -0500

    Blogging

--- javascripts/file-handler.js
+++ javascripts/file-handler.js
@@ -1,17 +1,23 @@
 var FileHandler = (function () {
 
 	var loadFile = function (event) {
-		console.log("ASSA");
 		var files = event.target.files; // FileList object
 
 		// files is a FileList of File objects. List some properties.
 		var output = [];
-		for (var i = 0, f; f = files[i]; i++) {
-			output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-				f.size, ' bytes, last modified: ',
-				f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-				'</li>');
-		}
+
+		var reader = new FileReader();
+		reader.onload = function () {
+			var arrayBuffer = reader.result;
+			console.log(arrayBuffer);
+		};
+		return reader.readAsText(files[0]);
+		//		for (var i = 0, f; f = files[i]; i++) {
+		//
+		//			console.log(files[i]);
+		//
+		//
+		//		}
 	}
 
 	return {
